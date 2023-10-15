1ebece0d3aa7865ed2553aefcbef02ade1c74a72
oleg.nazarenko.dev <oleg.nazarenko.dev@gmail.com>
2023-10-15 13:22:46 +0300

    Добавил шаблоны для кнопок.

---
+++ JCalculator/Converters/ClickColorConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace JCalculator.Converters
+{
+	[ValueConversion(typeof(Color), typeof(Color))]
+	public class ClickColorConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
